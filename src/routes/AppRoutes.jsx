91e47bc238a01130c7d7a5b76338dd322905dda3
Phyo-Khant-Kyaw <www.pkkcobra01@gmail.com>
2025-06-18 17:14:24 +0700

    fix case sensitive

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "../pages/front/Contact";
 import Login from "../pages/admin/auth/Login";
-import ContactList from "../pages/admin/Contact/ContactList";
+import ContactList from "../pages/admin/contact/ContactList";
 import { AuthProvider } from "../store/auth";
 import PrivateRoute from "./PrivateRoute";
 
